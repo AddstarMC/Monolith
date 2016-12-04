f9a317a1dec013687ecdc78e8c49ffc0cc76463d
Narimm <benjicharlton@gmail.com>
2016-12-05 00:59:37 +1000

    Update MonoSpawnEgg for 1.11 Changes re id:"minecraft:<SPAWNTYPE>"

--- src/main/java/au/com/addstar/monolith/MonoSpawnEgg.java
+++ src/main/java/au/com/addstar/monolith/MonoSpawnEgg.java
@@ -18,7 +18,6 @@ public class MonoSpawnEgg extends SpawnEgg {
     private String customName;
     private boolean customNameVisible;
 
-    @SuppressWarnings("deprecation")
     public MonoSpawnEgg(ItemStack egg) {
         this.egg = (SpawnEgg) egg.getData();
         this.item = egg;
@@ -31,7 +30,8 @@ public class MonoSpawnEgg extends SpawnEgg {
                 if (tag.hasKey("EntityTag") && tag.hasKeyOfType("EntityTag", 10)) {
                     NBTTagCompound entityTag = tag.getCompound("EntityTag");
                     if (entityTag.hasKeyOfType("id", 8)) {
-                        this.spawnType = EntityType.fromName(entityTag.getString("id"));
+                        String rawSpawnData = entityTag.getString("id").replace("minecraft:", "");
+                        this.spawnType = EntityType.fromName(rawSpawnData);
                     }
                     if (entityTag.hasKeyOfType("CustomName",8)){
                         this.customName = entityTag.getString("CustomName");
@@ -40,8 +40,9 @@ public class MonoSpawnEgg extends SpawnEgg {
                         this.customNameVisible = entityTag.getBoolean("CustomNameVisible");
                     }
                 }else{
+                    //no tag so create one
                     NBTTagCompound entityTag = new NBTTagCompound();
-                    entityTag.setString("id", EntityType.CHICKEN.getName());
+                    entityTag.setString("id", "minecraft:" + EntityType.CHICKEN.name());
                     tag.set("EntityTag", entityTag);
                     nmsStack.setTag(tag);
                     this.item = CraftItemStack.asBukkitCopy(nmsStack);
@@ -63,7 +64,12 @@ public class MonoSpawnEgg extends SpawnEgg {
         return this.customNameVisible;
     }
 
-    @SuppressWarnings("deprecation")
+    /**
+     * This sets a Mono Spawn Egg Type it also sets the NBT Data on the underlying item.
+     *
+     * @param type
+     * @return
+     */
     public boolean setMonoSpawnedType(EntityType type) {
         net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
         NBTTagCompound tag = nmsStack.getTag();
@@ -72,11 +78,11 @@ public class MonoSpawnEgg extends SpawnEgg {
                 if (tag.hasKey("EntityTag") && tag.hasKeyOfType("EntityTag", 10)) {
                     NBTTagCompound entityTag = tag.getCompound("EntityTag");
                     if (entityTag.hasKeyOfType("id", 8)) {
-                        entityTag.setString("id", type.getName());
+                        entityTag.setString("id", "minecraft:" + type.getName());
                     }
                 }else{
                     NBTTagCompound entityTag = new NBTTagCompound();
-                    entityTag.setString("id", type.getName());
+                    entityTag.setString("id", "minecraft:" + type.getName());
                     tag.set("EntityTag", entityTag);
                     nmsStack.setTag(tag);
                     this.item = CraftItemStack.asBukkitCopy(nmsStack);
@@ -91,10 +97,15 @@ public class MonoSpawnEgg extends SpawnEgg {
 
 }
 
+    /*
+    * Creates a 1.8 compatible spawn egg...
+     */
     public SpawnEgg getBukkitSpawnEgg(){
         egg.setSpawnedType(this.getMonoSpawnedType());
         return egg;
     }
 
-
+    public ItemStack getItem() {
+        return item;
+    }
 }
