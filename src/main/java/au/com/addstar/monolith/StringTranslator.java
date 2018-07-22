30df529af1314cab4df7b8b4216013ec25e9a782
Narimm <benjicharlton@gmail.com>
2018-07-22 11:25:07 +1000

    Add StringTranslator back to retrieve internal item and block names
    Signed-off-by: Narimm <benjicharlton@gmail.com>

---
+++ src/main/java/au/com/addstar/monolith/StringTranslator.java
@@ -0,0 +1,43 @@
+package au.com.addstar.monolith;
+
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_13_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_13_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_13_R1.inventory.CraftItemStack;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_13_R1.BlockPosition;
+import net.minecraft.server.v1_13_R1.IBlockData;
+
+public class StringTranslator
+{
+
+	
+	public static String getName(ItemStack item)
+	{
+		net.minecraft.server.v1_13_R1.ItemStack base = CraftItemStack.asNMSCopy(item);
+		if(base != null && base.getItem() != null)
+			return base.getName().getText();
+		return "Unknown";
+	}
+	
+	public static String getName(Entity entity)
+	{
+		net.minecraft.server.v1_13_R1.Entity base = ((CraftEntity) entity).getHandle();
+		return base.getName();
+	}
+
+	/**
+	 * REturns the minecraft name of a block as per the interal registry
+	 * @param block
+	 * @return
+	 */
+	public static String getName(Block block)
+	{
+		BlockPosition pos = new BlockPosition(block.getX(), block.getY(), block.getZ());
+		IBlockData type = ((CraftWorld)block.getWorld()).getHandle().getType(pos);
+		return net.minecraft.server.v1_13_R1.Block.REGISTRY.b(type.getBlock()).toString();
+	}
+}
